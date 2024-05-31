676cd665f208b815241e90c1ec81e2c99b23ab7a
gilbmporto <gilbmp@gmail.com>
2024-05-31 13:46:29 -0300

    Set application to run at Heroku

--- db/conn.js
+++ db/conn.js
@@ -1,20 +1,30 @@
 const { Sequelize } = require("sequelize")
 require("dotenv").config()
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.MYSQL_ROOT_PASSWORD,
-  {
-    host: "localhost",
-    dialect: "mysql",
-    port: 3306,
-    pool: {
-      max: 10,
-      min: 0,
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.MYSQL_ROOT_PASSWORD,
+//   {
+//     host: "localhost",
+//     dialect: "mysql",
+//     port: 3306,
+//     pool: {
+//       max: 10,
+//       min: 0,
+//     },
+//   }
+// )
+
+const sequelize = new Sequelize(process.env.JAWSDB_URL, {
+  dialect: "mysql",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
-  }
-)
+  },
+})
 
 try {
   sequelize.authenticate()
