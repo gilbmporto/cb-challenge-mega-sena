b8ffeabcae9d3e81ec7a1d5461b2cc1e89d3bf94
gilbmporto <gilbmp@gmail.com>
2024-05-31 15:31:52 -0300

    Changed results style (li) again and added new hbs helper function

--- index.js
+++ index.js
@@ -26,6 +26,9 @@ const hbs = exphbs.create({
     or: () => {
       return Array.prototype.slice.call(arguments, 0, -1).some(Boolean)
     },
+    join: (array, separator) => {
+      return array.join(separator)
+    },
   },
 })
 
